ea8557333a1fca5ee160b8f30427ba21ce33936c
Vicky Poonia <vickychaudhary4596@gmail.com>
2024-07-11 13:00:35 +0530

    done

--- patterns.cpp
+++ patterns.cpp
@@ -125,17 +125,17 @@ int main(){
         //   ***
         //    *
 int n=4;      
-// for(int i=1;i<=n;i++){
-//     for(int j=1;j<=n-i;j++){
-//         cout<<" ";
-//     }
-//     for(int j=1;j<=2*i-1;j++){
-//         cout<<"*";
-//     }
-//     cout<<endl;
-// }
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    for(int j=1;j<=2*i-1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
 for(int i=n; i>=1;i--){
-    for(int j=1;j<=n-1;j++){
+    for(int j=i;j<=n-1;j++){
         cout<<" ";
     }
     for(int j=1;j<=2*i-1;j++){
